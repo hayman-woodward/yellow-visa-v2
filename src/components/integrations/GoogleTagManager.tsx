e5976409f5fef1c7c1ea43559399a201820273c1
kpaxstoregit <kpaxstore@gmail.com.br>
2025-10-27 09:48:58 -0300

    correcoes analytcs

--- src/components/integrations/GoogleTagManager.tsx
+++ src/components/integrations/GoogleTagManager.tsx
@@ -6,6 +6,11 @@ import { useEffect } from 'react';
 declare global {
   interface Window {
     dataLayer: Array<Record<string, unknown>>;
+    gtag?: (...args: unknown[]) => void;
+    fbq?: (...args: unknown[]) => void;
+    analytics?: {
+      track: (event: string, properties?: Record<string, unknown>) => void;
+    };
   }
 }
 
