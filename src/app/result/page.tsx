8751040d60b49c6eab1c9733ac9c8217c90df86c
kpaxstoregit <kpaxstore@gmail.com.br>
2025-10-30 11:19:12 -0300

    voltando fallback

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import ResultadoPage from '@/app/comecar/components/08-captura-lead/ResultadoPage';
 
 export default function ResultPage() {
-  return <ResultadoPage />;
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+    <ResultadoPage />
+  </Suspense>
+  );
 }
