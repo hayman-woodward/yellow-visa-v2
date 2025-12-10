9e11ba1506fd7eccfaa108824fb4d71588c785d8
Hayhyve IT <it@hayhyve.com>
2025-12-10 14:26:25 -0300

    fix: corrigir imagens da hero sobre para hero-yv-sobre

--- src/app/(site)/sobre/components/heroSobre.tsx
+++ src/app/(site)/sobre/components/heroSobre.tsx
@@ -5,10 +5,12 @@ export default function HeroSobre() {
   return (
     <div className='relative'>
       <YVBanner
-        src='/imgs/sobre/desktop/sobre-hero.png'
-        srcMobile='/imgs/sobre/desktop/sobre-hero.png'
-        alt='Hero Guia Do Imigrante'
+        src='/imgs/sobre/desktop/hero-yv-sobre.jpg'
+        src2x='/imgs/sobre/desktop/hero-yv-sobre@2x.jpg'
+        srcMobile='/imgs/sobre/desktop/hero-yv-sobre.jpg'
+        alt='Hero Sobre'
         className='lg:max-h-none h-[calc(100vh+10px)] lg:h-screen max-h-[calc(100vh+10px)] hero-max-height -mt-[88px]'
+        priority
       />
       {/* Scroll Button */}
       <div className='absolute bottom-0 left-1/2 transform -translate-x-1/2 z-20'>
